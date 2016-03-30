5542427858d443b312ce90d706cf27aaaecc68eb
Ginelle <gnlzignacio@gmail.com>
2016-03-30 22:53:25 +0200

    Fjernet størrelsen på main
    Sånn at vinduet følger scenebuilder sin størrelse

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         primaryStage.setTitle("Game of Life");
-        primaryStage.setScene(new Scene(root, 900, 638));
+        primaryStage.setScene(new Scene(root));
 
         primaryStage.show();
     }
