91963de52070a92f1b970ab408e08ba94bd57913
Ginelle <gnlzignacio@gmail.com>
2016-04-01 12:23:30 +0200

    Oppdatert versjon av JavaDocs og Game rules vindu

--- src/sample/FileReader.java
+++ src/sample/FileReader.java
@@ -2,12 +2,37 @@ package sample;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by RudiAndre on 30.03.2016.
  */
 public class FileReader {
+
+    /**
+     * Reading file from disk
+     * @param file
+     * @throws IOException
+     */
     public void readGameBoardFromDisk(File file) throws IOException {
         //readGameBoard(new FileReader(file));
     }
+
+    /**
+     * Reading file from web
+     * @param url
+     * @throws IOException
+     * @throws PatternFormatException
+     */
+
+    /*
+    public void readGameBoardFromURL(String url) throws IOException,
+            PatternFormatException {
+            URL destination = new URL(url);
+            URLConnection conn = destination.openConnection();
+            readGameBoard(new InputStreamReader(conn.getInputStream()));
+    }*/
+
 }
---
+++ src/sample/PatternFormatException.java
@@ -0,0 +1,8 @@
+package sample;
+
+/**
+ * Created by Ginelle on 4/1/2016.
+ */
+public class PatternFormatException extends Exception {
+
+}
