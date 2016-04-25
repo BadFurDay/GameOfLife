b7ed1b383ee312f1f46d79e5f3139a5cc2960112
ginellezi <gnlzignacio@gmail.com>
2016-04-25 10:57:31 +0200

    Pattern format exceptions

---
+++ src/sample/PatternFormatExceptions.java
@@ -0,0 +1,33 @@
+/**
+ * This class is responsible for catching exceptions
+ * related to the RLE format which makes it possible
+ * for the user to upload files containing the pattern.
+ *
+ * @author Ginelle Ignacio
+ */
+
+package sample;
+
+
+public class PatternFormatExceptions extends Exception {
+
+
+    /**
+     * Pattern Format Exception has a default constructor
+     * that receives no arguments.
+     */
+    public PatternFormatExceptions(){
+
+    }
+
+    /**
+     * Constructor receives a string message of the error
+     * that occurred
+     *
+     * @param message String message of the exception
+     */
+    public PatternFormatExceptions(String message){
+        super(message);
+    }
+
+}
